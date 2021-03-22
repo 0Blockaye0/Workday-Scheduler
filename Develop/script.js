a5d0de4786955b768b5b185660dbe39a75c3e684
_blockaye_ <blake_austinjohnson@icloud.com>
2021-03-21 19:55:10 -0500

    bootstrap classes added to timeblocks for past,present,future.

--- Develop/script.js
+++ Develop/script.js
@@ -4,42 +4,100 @@ var currentDate = moment().format("dddd, MMMM Do YYYY");
 console.log(currentDate);
 currentDateEl.innerHTML = currentDate;
 
-//DATA
+// CURRENT TIME
+var currentTime = moment().hour();
+console.log(currentTime);
+
+// DATA
 var schedule = [];
 
+// LOAD EVENTS ON PAGE-LOAD
 var load = function() {
     schedule = JSON.parse(localStorage.getItem("schedule"));
-    console.log(schedule);
-    //loop over object array and fill time-blocks with events
+
+        if(!schedule){
+            console.log("nothing in storage");
+            window.alert("Your schedule is wiiiiiiide open");
+            schedule = [];
+        } 
+        else {
+            schedule = JSON.parse(localStorage.getItem("schedule"));
+            console.log(schedule);
+            //loop over objects in array and fill time-blocks with events
+            schedule.forEach(event => {
+                console.log(event);
+                console.log(event.index);
+                console.log($("#" + event.index));
+                var timeBlock = $("#" + event.index);
+
+                timeBlock.val(event.text);
+            });
+        };
 };
 
+// SAVE EVENTS
 var saveSchedule = function() {
+    localStorage.clear();
     localStorage.setItem("schedule", JSON.stringify(schedule));
   };
 
-// SAVE EVENT/ SAVE BUTTON
+// SAVE EVENTS/ SAVE BUTTON
 $(".saveBtn").on("click", function() {
-    
-    for (var i = 0; i < 5; i++){
-        var eventText = $("#" + i ).val();
-        var eventHour = $("#b" + i).html();
-        console.log(eventText);
-        console.log(eventHour);
 
+    schedule = [];
+    
+    for (var i = 9; i < 17; i++){
+        var eventText = $("#" + i ).val();
+        console.log(eventText);
+      
         var event = {
+            
             index: i,
-            hour: eventHour,
+           
             text: eventText
         };
         console.log(event);
-
+        
         schedule.push(event);
         console.log(schedule);
-
-        // schedule.forEach
     }
     saveSchedule();
 });
 
-// LOAD EVENTS
-//load();
+// ADDS COLOR WARNINGS TO EVENTBLOCKS
+var timeWarning = function() {
+    
+    for (var i = 9; i < 17; i++){
+        var timeblock = $("#" + i );
+        if (currentTime > i ) {
+            console.log(timeblock);
+            timeblock.addClass("alert alert-secondary");
+        } else if (currentTime === i ){
+            timeblock.addClass("alert alert-danger");
+        } else {
+            timeblock.addClass("alert alert-success");
+        };
+        
+        
+
+
+
+        // var eventBlock = document.getElementById(i);
+        // console.log(eventBlock);
+
+        // if (currentTime === timeText){
+        //     eventBlock.classList.add("alert", "alert-success");
+        // } else 
+        // if (currentTime.isafter(timeText)) {
+        //     console.log("it worked");
+        //     eventBlock.classList.add("alert", "alert-danger");
+        // }
+    };
+};    
+
+// LOAD THE CALENDER
+load();
+
+timeWarning();
+
+setInterval(function(){ timeWarning, 60000});
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+-this app allows the user to input events in aech hour of the day
+
+-the time blocks will change color to represent past, present and future.
+
+-all events saved everytime a user presses any of the save buttons
+
+the color warnings will update every minute so that the user is sure to have the current hour highlighted correctly
+
+-link:
+
